fc53e6780447d5a0b8d2d0f0678e8c9840c6c9f7
choikangjae <choikj33@gmail.com>
2023-03-11 17:04:45 +0900

    feat: Processor architecture

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+const WIDTH: u8 = 64;
+const HEIGHT: u8 = 32;
+
+struct Processor {
+    ram: [u8; 4096],
+    registers: [u8; 16],
+    i: u16,
+    delay_timer: u8,
+    sound_timer: u8,
+    pc: u16,
+    sp: u8,
+    stack: [u16; 16],
+    display: [bool; 64 * 32],
+    key: [u8; 16],
+}
+
+//delta somewhere
+
 fn main() {
-    println!("Hello, world!");
+    //init processor.
+
+    //load binary.
+
+    //loop
+        //opcode
+
+        //render
 }
